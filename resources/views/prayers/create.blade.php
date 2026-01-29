de4d2c88cbfa35f1d7281e05f7e1ae5e87a70eb1
Kuttoh Isaac <kuttohisaac@gmail.com>
2026-01-29 14:30:16 +0300

    ref fix

--- resources/views/prayers/create.blade.php
+++ resources/views/prayers/create.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.guest-app :title="'Submit Prayer'">
     <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
         <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-es-lg rounded-ee-lg lg:rounded-ss-lg lg:rounded-ee-none flex flex-col items-center justify-center text-center">
-            @livewire('create-prayer')
+            @livewire('prayers.create-prayer')
         </div>
     </main>
 </x-layouts.guest-app>
